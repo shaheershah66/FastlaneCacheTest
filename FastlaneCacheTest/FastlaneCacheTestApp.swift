f0965eae7defc547d95acec162aea89d2f3fe633
shaheer <shaheertabassum@gmail.com>
2025-08-08 19:25:34 +0500

    Initial Commit

---
+++ FastlaneCacheTest/FastlaneCacheTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneCacheTestApp.swift
+//  FastlaneCacheTest
+//
+//  Created by Hafiz Arsalan Muhammad on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneCacheTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios
+
+target 'FastlaneCacheTest' do
+  use_frameworks!
+  pod 'SnapKit', '~> 5.6'
+end
+
